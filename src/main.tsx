0aabfc6169db3ce8b793fdb129e3fa71799d6edf
LearnwithDSP <ramonbisola1@gmail.com>
2026-09-04 22:26:31 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import { ToastProvider } from './context/ToastContext.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { ToastProvider } from './context/ToastContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
